1901c5035889074fece31a3d2ce322f66a5073f8
CBellotti96 <cbellott@udel.edu>
2018-03-21 16:11:44 -0400

    finished

--- philosophers.c
+++ philosophers.c
@@ -1,3 +1,5 @@
+//Chris Bellotti
+
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,7 +13,7 @@ int forks[NUM_PHILOSOPHERS];
 
 void pickup_forks(int philosopher_number, int left_fork, int right_fork){
   pthread_mutex_lock(&mutex);
-  while(forks[left_fork] || forks[right_fork])
+  while(forks[left_fork] || forks[right_fork]) //if either forks are taken wait until they're free
     pthread_cond_wait(&cond_var, &mutex);
   printf("Philosopher %d taking forks and eating.\n", philosopher_number);
   fflush(stdout);
@@ -39,11 +41,11 @@ void * philosopher(void *args){
   int right = i;
   int left = (i - 1 == -1) ? NUM_PHILOSOPHERS - 1 : (i - 1);
   int loops_left = 3;
-  while(loops_left > 0){
+  while(loops_left > 0){ //looping so each philosopher eats 3 times before program ends
     pickup_forks(i, left, right);
     sleep(1);
     return_forks(i, left, right);
-    sleep((random() % 3) + 1);
+    sleep((random() % 3) + 1); //think for a while after eating
     loops_left--;
   }
 
@@ -53,14 +55,14 @@ int main(int argc, char**argv){
   pthread_mutex_init(&mutex, NULL);
   pthread_cond_init(&cond_var, NULL);
   int i;
-  for(i=0; i<NUM_PHILOSOPHERS; i++)
+  for(i=0; i<NUM_PHILOSOPHERS; i++) //initializing forks
   forks[i] = 0;
-  for(i=0; i<NUM_PHILOSOPHERS; i++){
+  for(i=0; i<NUM_PHILOSOPHERS; i++){ //creating thread for each philosopher
     int *arg = malloc(sizeof(*arg));
     *arg = i;
     pthread_create( &philosophers[i], NULL, philosopher, arg);
   }
   for(i=0; i<NUM_PHILOSOPHERS; i++)
-    pthread_join(philosophers[i],NULL);
+    pthread_join(philosophers[i],NULL); //join threads at the end and exit
   return 0;
 }
